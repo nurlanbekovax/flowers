fbf7f240d9d4d59d9ce73962c9f8c9055260268c
Nurshat <130434219+nurlanbekovax@users.noreply.github.com>
2024-02-18 21:11:24 +0600

    first commit with base settings

---
+++ src/Components/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "./Navbar";
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from "../Assets/home-banner-image.png";
+import { FiArrowRight } from "react-icons/fi";
+
+const Home = () => {
+    return (
+    <div className="home-container">
+        <Navbar/>
+        <div className="home-banner-container">
+          <div className="home-bannerImage-container">
+            <img src={BannerBackground} alt="" />
+            </div>
+            <div className="home-text-section">
+                <h1 className="primary-heading"></h1>
+                h1
+                </div>
+        </div>
+    </div>
+    );
+};
+
+export default Home;
